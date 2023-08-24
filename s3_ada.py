c17cd907fd7a927adfee0c30ad2342bbcf80ec9e
anabferraz <ferraz.anab@gmail.com>
2023-08-23 23:50:12 -0300

    .

--- s3_ada.py
+++ s3_ada.py
@@ -1,7 +1,7 @@
-import boto3 as b
+import boto3
 
 def list_s3():
-    s3_resource = b.resource('s3')
+    s3_resource = boto3.resource('s3')
     print("Listing my buckets:")
     for bucket in s3_resource.buckets.all():
         print(f"\t{bucket.name}")
