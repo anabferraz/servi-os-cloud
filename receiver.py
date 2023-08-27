db826a78654e3a753b81ac2d1b45c996fd27c137
anabferraz <ferraz.anab@gmail.com>
2023-08-26 21:04:20 -0300

    receiver included

---
+++ receiver.py
@@ -0,0 +1,17 @@
+import boto3
+
+def consume_queue():
+    sqs_client = boto3.client('sqs', region_name='us-east-2')
+    response = sqs_client.receive_message(
+        QueueUrl="https://sqs.us-east-2.amazonaws.com/248735089137/QueueTest",
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=10
+    )
+    
+    for message in response.get('Messages', []):
+        body = message.get('Body', 'No body')
+        print(body)
+
+if __name__ == '__main__':
+
+    response = consume_queue()
