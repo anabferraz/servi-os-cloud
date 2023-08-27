ae70c4b82706d73c94857ab761a04dd68fc02911
anabferraz <ferraz.anab@gmail.com>
2023-08-27 01:23:26 -0300

    almost ready

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from sqs import send_message
 from receiver import consume_queue
 
 def main():
-    print("Bem-vindo(a) ao Controle AWS da Nuclea! Vamos começar criando o seu bucket.")
+    print("Bem-vindo(a) ao Controle de relatórios AWS da Nuclea! Vamos começar criando o seu bucket.")
     create_s3()
     upload_file_s3()
     list_s3()
     send_message()
     consume_queue()
-    print("Obrigado(a) por utilizar o Controle AWS da Núclea! Volte sempre! :D")
+    print("Obrigado(a) por utilizar o Controle de relatórios AWS da Núclea! Volte sempre! :D")
 
 if __name__ == '__main__':
 
